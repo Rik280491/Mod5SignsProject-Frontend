7a0abe0948ae1d987cab301543325df3682a83d7
RikBala <rik280491@googlemail.com>
2020-05-13 17:57:08 +0100

    initial setup

---
+++ src/homePage/HomePageContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import SignCard from '../signs/SignCard'
+
+export default class HomePageContainer extends React.Component {
+    constructor(){
+        super()
+
+    }
+
+   
+
+
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/signs/SignCard.js
@@ -0,0 +1,13 @@
+// functional component 
+
+import React from 'react'
+
+
+export default function SignCard({}){
+   
+   
+    return(
+        
+        <div>Hello</div>
+    )
+}
