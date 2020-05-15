a34855bf929d61aaf4d9240dd2be3f6aae91fda8
RikBala <rik280491@googlemail.com>
2020-05-15 21:24:03 +0100

    approach this in the morning

--- src/upload/UploadVideo.js
+++ src/upload/UploadVideo.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import SignCard from "../signs/SignCard";
-import API from "../API/API"
+import API from "../API/API";
 
-function UploadVideo(props) {
+function UploadVideo({ username }) {
 	const [loading, setLoading] = useState(false);
-    const [video, setVideo] = useState(null);
-    const[signName, setSignName] = useState("")
-    const [newSign, setNewSign] = useState(null)
+	const [video, setVideo] = useState(null);
+	const [signName, setSignName] = useState("");
+	const [newSign, setNewSign] = useState(null);
 
 	const handleChange = async (e) => {
 		const files = e.target.files;
@@ -27,35 +27,38 @@ function UploadVideo(props) {
 
 		const file = await response.json();
 		setVideo(file.secure_url);
-        setLoading(false);
-        
-        
-    };
-    
-    const handleNameChange = (e) => {
-        setSignName(e.target.value)
-        
-    }
+		setLoading(false);
+	};
 
-    const handlePost = () => {
-        API.createSign({
-            name: signName
-        }).then(sign => setNewSign(sign))
-        
-    }
-    
-    const handleVideoPost = () => {
-       console.log(newSign)
-        API.createVideo({
-            video_url: video,
-            sign: newSign
-        }, localStorage.token ).then(video => console.log(video))
-    }
+	const handleNameChange = (e) => {
+		setSignName(e.target.value);
+	};
 
+	const handlePost = () => {
+		API.createSign({
+			name: signName,
+		}).then((sign) => setNewSign(sign));
+        console.log(newSign)
+        // API.createVideo(
+		// 	{
+		// 		video_url: video,
+		// 		sign: newSign (this is null)
+		// 	},
+		// 	localStorage.token
+		// ).then((video) => console.log(video));
+	};
+
+	const handleVideoPost = () => {
+	   console.log(newSign)
+	    API.createVideo({
+	        video_url: video,
+	        sign: newSign
+	    }, localStorage.token ).then(video => console.log(video))
+	}
 
 	return (
 		<div>
-			{!props.username ? (
+			{!username ? (
 				<h1>
 					To upload a video please <Link to="/login"> log in </Link> or
 					<Link to="/signup"> Sign Up </Link>{" "}
@@ -63,7 +66,12 @@ function UploadVideo(props) {
 			) : (
 				<>
 					<h1>Upload a Video</h1>
-					<input type="text" name="signName" placeholder="Name of Sign" onChange={handleNameChange}  />
+					<input
+						type="text"
+						name="signName"
+						placeholder="Name of Sign"
+						onChange={handleNameChange}
+					/>
 					<input
 						type="file"
 						name="file"
@@ -74,13 +82,12 @@ function UploadVideo(props) {
 					{loading ? (
 						<h2>Loading...{video}</h2>
 					) : (
-                        // better loading icon, progress bar?
-                        <>
-						<SignCard name={signName} videoURL={video} />
-                        <button onClick={handlePost}>POST SIGN</button>
-                        <button onClick={handleVideoPost}>POST VIDEO</button>
-                        
-                        </>
+						// better loading icon, progress bar?
+						<>
+							<SignCard name={signName} videoURL={video} />
+							<button onClick={handlePost}>UPLOAD</button>
+                            <button onClick={handleVideoPost}>TEST</button>
+						</>
 					)}
 				</>
 			)}
@@ -92,7 +99,5 @@ const mapStateToProps = (state) => {
 		username: state.username,
 	};
 };
-
-
 
 export default connect(mapStateToProps, null)(UploadVideo);
