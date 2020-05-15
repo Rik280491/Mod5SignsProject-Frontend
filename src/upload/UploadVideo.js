0f6715ca3cd502e802bb3a6e929329d7f6243c55
RikBala <rik280491@googlemail.com>
2020-05-15 14:32:05 +0100

    video upload worksss

--- src/upload/UploadVideo.js
+++ src/upload/UploadVideo.js
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
-import Login from "../login/Login";
-import Signup from "../signup/Signup";
+import SignCard from '../signs/SignCard'
 
-class UploadVideo extends React.Component {
-	render() {
-		console.log(this.props);
+
+function UploadVideo(props) {
+    
+    const [loading, setLoading] = useState(false)
+    const [video, setVideo] = useState("")
+
+    const handleChange = async e => {
+       
+        const files = e.target.files 
+        const data = new FormData()
+        data.append('file', files[0])
+        data.append('upload_preset', 'SignVideos')
+        setLoading(true)
+
+        const response = await fetch("https://api.cloudinary.com/v1_1/dm0musx2l/video/upload", {
+            method: 'POST',
+            body: data
+        })
+
+        const file = await response.json()  
+        console.log(file)
+        setVideo(file.secure_url)
+        setLoading(false)
+        console.log(video)
+    }
+
+   
+    
+    
+    
+    
+    
+
 		return (
+            
 			<div>
-				{!this.props.username ? (
+				{!props.username ? (
 					<h1>
 						
 						To upload a video please <Link to="/login"> log in </Link> or
 						<Link to="/signup"> Sign Up </Link>{" "}
 					</h1>
 				) : (
-					<h1>Upload a Video</h1>
-				)}
+					<>
+                    <h1>Upload a Video</h1>
+                    <input type="text" name="name" />
+                    <input type="file" name="file" placeholder="Upload a Video" onChange={handleChange}/>
+                    
+                
+                    { loading ? 
+                        <h2>Loading...{video}</h2>
+                        // better loading icon, progress bar? 
+                    :
+                    
+                       <SignCard videoURL={video}/>
+                }
+                
+                
+                
+                
+                </>
+                )}
 			</div>
 		);
-	}
+	
 }
 const mapStateToProps = (state) => {
 	return {
