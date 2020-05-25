6d087c0beb8331d5922bc64a7663a1f0d1144baf
RikBala <rik280491@googlemail.com>
2020-05-25 15:15:11 +0100

    changed text colour of search field to be more accesible. higher contrast

--- src/search/DropdownSearch.js
+++ src/search/DropdownSearch.js
@@ -2,29 +2,45 @@ import React from "react";
 import { connect } from "react-redux";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  inputRoot: {
+    color: "white",
+  
+  }
+}));
+
 
 function DropdownSearch(props) {
 	const { signs, onChange, speechPlaceholder} = props;
+  const classes = useStyles();
 
   console.log(speechPlaceholder)
 	return (
-		<div style={{ width: 150, height: 70 }}>
+		<div style={{ width: 150, height: 70}}>
 			<Autocomplete
       size="small"
 				id="free-solo-demo"
 				freeSolo
         autoComplete
+        classes={classes}
+
         
 				onChange={onChange}
 				options={signs.map((option) => option.name)}
 				renderInput={(params) => (
 					<TextField
-						{...params}
+             {...params}
+            
+            
 						label="Search"
 						margin="normal"
-						variant="outlined"
+            variant="outlined"
+            
             // onChange={onChange}
             // placeholder={speechPlaceholder}
+          
 					/>
 				)}
 			/>
